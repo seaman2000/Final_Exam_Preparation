7b6e4dc347d68b10d0bcfa7b375cec00f2fdfa6d
Georgi Angelov <gtangelov76@gmail.com>
2026-02-24 18:52:43 +0200

    Update the_imitation_game.py

--- Exam_Prep1/the_imitation_game.py
+++ Exam_Prep1/the_imitation_game.py
@@ -1,18 +1,28 @@
+def move(letters_number: int, message:list) -> list:
+    return message[letters_number:] + message[:letters_number]
 
+def insert(idx: int, value_: str, message:list) -> list:
+    message.insert(idx, value_)
+    return message
+def change_all(substring, replacement, message):
+    pass
 
 
 
 
 encrypted_message = input()
-
+encrypted_message = list(encrypted_message)
 while True:
     command = input()
     if command == "Decode":
         break
     parts = command.split("|")
     if parts[0] == "Move":
-        pass
+        number_of_letters = int(parts[1])
+        encrypted_message = move(number_of_letters, encrypted_message)
     elif parts[0] == "Insert":
-        pass
+        index = int(parts[1])
+        value = parts[2]
+        encrypted_message = insert(index, value, encrypted_message)
     elif parts[0] == "ChangeAll":
         pass
