1ca0ed33de1abe992f3c669c60bbecb6e0bf4d88
Georgi Angelov <gtangelov76@gmail.com>
2026-02-24 18:36:52 +0200

    added task

--- Exam_Prep1/the_imitation_game.py
+++ Exam_Prep1/the_imitation_game.py
@@ -1,1 +1,18 @@
+
+
+
+
+
 encrypted_message = input()
+
+while True:
+    command = input()
+    if command == "Decode":
+        break
+    parts = command.split("|")
+    if parts[0] == "Move":
+        pass
+    elif parts[0] == "Insert":
+        pass
+    elif parts[0] == "ChangeAll":
+        pass
