ce2a165d4ad5793b34dd79acada83cc53b0871af
Georgi Angelov <gtangelov76@gmail.com>
2026-03-21 19:13:59 +0200

    Update plant_discovery.py

--- Exam_Prep2/plant_discovery.py
+++ Exam_Prep2/plant_discovery.py
@@ -1,22 +1,25 @@
 def rate(collection: dict, plant_type: str, rating: int):
     if plant_type not in collection:
-        return "error"
+        return False
 
     collection[plant_type][1].append(rating)
+    return True
 
 
 def update(collection: dict, plant_type: str, rarity: int):
     if plant_type not in collection:
-        return "error"
+        return False
 
     collection[plant_type][0] = rarity
+    return True
 
 
 def reset(collection: dict, plant_type: str):
     if plant_type not in collection:
-        return "error"
+        return False
 
     collection[plant_type][1] = []
+    return True
 
 
 
@@ -29,29 +32,42 @@ plants = {}
 for _ in range(number_of_lines):
     current_plant = input()
     plant, rarity = current_plant.split("<->")
-    plants[plant] = int(rarity)
+    rarity = int(rarity)
+
+    if plant not in plants:
+        plants[plant] = [rarity, []]
+    else:
+        plants[plant][0] = rarity
 
 command = input()
-pattern = r'[A-Za-z]+'
+pattern = r'[A-Za-z]+|\d+'
+is_valid = True
 
 while command != "Exhibition":
     parts = re.findall(pattern, command)
     type_of_command = parts[0]
     plant = parts[1]
+    result = None
 
     if type_of_command == "Rate":
         rating = int(parts[2])
-        plants = rate(plants, plant, rating)
+        result = rate(plants, plant, rating)
 
     elif type_of_command == "Update":
         new_rarity = int(parts[2])
-        plants = update(plants, plant, new_rarity)
+        result = update(plants, plant, new_rarity)
 
     elif type_of_command == "Reset":
-        plants = reset(plants, plant)
+        result = reset(plants, plant)
+
+    if result == "error":
+        print("error")
+
+    command = input()
 
 print(f"Plants for the exhibition:")
 for plant, values in plants.items():
     rarity, rating = values
+
     avg_rating = sum(rating) / len(rating)
     print(f"- {plant}; Rarity: {rarity}; Rating: {avg_rating:.2f}")
