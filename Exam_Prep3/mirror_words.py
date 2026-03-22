e67c9076c70054ce44fac8877ed5ee1718ee6d97
Georgi Angelov <gtangelov76@gmail.com>
2026-03-22 16:11:40 +0200

    Update mirror_words.py

--- Exam_Prep3/mirror_words.py
+++ Exam_Prep3/mirror_words.py
@@ -3,23 +3,23 @@ import re
 text = input()
 pattern = r"(@|#)([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1"
 
-word_collection = {}
+word_collection = []
 words = re.findall(pattern, text)
-words_without_symbols = [w[1] for w in words]
+words_without_symbols = [(first, second) for _, first, second in words]
 
 if not words_without_symbols:
     print("No word pairs found!")
 else:
     print(f"{len(words_without_symbols)} word pairs found!")
-    for first, second in zip(words_without_symbols, words_without_symbols[1:]):
+    for (first, second) in words_without_symbols:
         if first == second[::-1]:
-            word_collection[first] = second
+            word_collection.append(f"{first} <=> {second}")
 
 if not word_collection:
     print(f"No mirror words!")
 else:
-    print(f"The mirror words are:")
-    for first, second in word_collection.items():
-        print(f"{first} <=> {second}", end=", ")
+    print("The mirror words are:")
+    print(", ".join(word_collection))
+
 
 
