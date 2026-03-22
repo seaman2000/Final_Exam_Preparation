8a88a100696470f9434ea9ecde8929dd4aa24d36
Georgi Angelov <gtangelov76@gmail.com>
2026-03-22 14:56:01 +0200

    Update secret_chat.py

--- Exam_Prep3/secret_chat.py
+++ Exam_Prep3/secret_chat.py
@@ -4,16 +4,14 @@ def insert_space(message:str, idx:int):
 
 
 def reverse(message: str, sub_string: str):
-    if not sub_string in message:
-        return None
     idx_of_sub = message.find(sub_string)
     message = message[:idx_of_sub] + sub_string[::-1]
     return message
 
 
 def change_all(message: str, sub_string, new_string):
-    message.replace(sub_string, new_string)
-    return message
+    new_message = message.replace(sub_string, new_string)
+    return new_message
 
 
 concealed_message = input()
@@ -25,23 +23,22 @@ while command != "Reveal":
 
     if type_of_command == "InsertSpace":
         index = int(parts[1])
-        result = insert_space(concealed_message, index)
+        concealed_message = insert_space(concealed_message, index)
         print(concealed_message)
 
     elif type_of_command == "Reverse":
         substring = parts[1]
-        result = reverse(concealed_message, substring)
-        if result:
+        if substring not in concealed_message:
             print("error")
         else:
+            concealed_message = reverse(concealed_message, substring)
             print(concealed_message)
-
     elif type_of_command == "ChangeAll":
         substring = parts[1]
         replacement = parts[2]
-        result = change_all(concealed_message, substring, replacement)
+        concealed_message = change_all(concealed_message, substring, replacement)
         print(concealed_message)
 
     command = input()
 
-print(f"You have a new text message: {result}")
+print(f"You have a new text message: {concealed_message}")
