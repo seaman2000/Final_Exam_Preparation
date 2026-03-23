f1a99e9b42581ad3bbd4d4e0eaadf143bf05725a
Georgi Angelov <gtangelov76@gmail.com>
2026-03-23 18:48:17 +0200

    Update password_reset.py

--- Exam_Prep4/password_reset.py
+++ Exam_Prep4/password_reset.py
@@ -1,32 +1,33 @@
 given_password = input()
 command = input()
-current_password = ""
 
 while command != "Done":
     parts = command.split()
     type_of_command = parts[0]
 
     if type_of_command == "TakeOdd":
+        current_password = ""
         for idx in range(len(given_password)):
             if idx % 2 == 1:
                 current_password += given_password[idx]
-        print(current_password)
+        given_password = current_password
+        print(given_password)
 
     elif type_of_command == "Cut":
         index = int(parts[1])
         length = int(parts[2])
-        current_password = current_password[:index] + current_password[index + length:]
-        print(current_password)
+        given_password = given_password[:index] + given_password[index + length:]
+        print(given_password)
 
     elif type_of_command == "Substitute":
         substring = parts[1]
         substitute = parts[2]
-        if not substring in current_password:
+        if not substring in given_password:
             print("Nothing to replace!")
         else:
-            current_password = current_password.replace(substring, substitute)
-            print(current_password)
+            given_password = given_password.replace(substring, substitute)
+            print(given_password)
 
     command = input()
 
-print(f"Your password is: {current_password}")
+print(f"Your password is: {given_password}")
