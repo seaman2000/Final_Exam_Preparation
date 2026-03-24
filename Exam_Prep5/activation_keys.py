89ff1d65a31910144153fa70021d41cc25682dcf
Georgi Angelov <gtangelov76@gmail.com>
2026-03-24 12:56:40 +0200

    Update activation_keys.py

--- Exam_Prep5/activation_keys.py
+++ Exam_Prep5/activation_keys.py
@@ -13,9 +13,9 @@ def flip_upper_and_lower(activation_key: str, upper_lower: str, start: int, end:
     return activation_key[:start] + sub_string + activation_key[end:]
 
 
-def slice_():
-    pass
-
+def slice_(activation_key: str, start: int, end: int):
+    activation_key = activation_key[:start] + activation_key[end + 1:]
+    return activation_key
 
 
 raw_activation_key = input()
@@ -24,6 +24,7 @@ command = input()
 while command != "Generate":
     parts = command.split(">>>")
     action = parts[0]
+    
     if action == "Contains":
         substring = parts[1]
         print(contains(raw_activation_key, substring))
@@ -32,5 +33,15 @@ while command != "Generate":
         upper_or_lower = parts[1]
         start_idx = int(parts[2])
         end_idx = int(parts[3])
+        raw_activation_key = flip_upper_and_lower(raw_activation_key, upper_or_lower, start_idx, end_idx)
+        print(raw_activation_key)
+
+    elif action == "Slice":
+        start_idx = int(parts[1])
+        end_idx = int(parts[2])
+        raw_activation_key = slice_(raw_activation_key, start_idx, end_idx)
+        print(raw_activation_key)
 
     command = input()
+
+print(f"Your activation key is: {raw_activation_key}")
