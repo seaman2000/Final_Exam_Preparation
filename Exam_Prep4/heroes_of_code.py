4c6982df4080b1a765944692aab331c686070109
Georgi Angelov <gtangelov76@gmail.com>
2026-03-24 11:33:55 +0200

    Update heroes_of_code.py

--- Exam_Prep4/heroes_of_code.py
+++ Exam_Prep4/heroes_of_code.py
@@ -1,3 +1,22 @@
+def cast_spell():
+    pass
+
+
+def take_damage():
+    pass
+
+
+def recharge():
+    pass
+
+
+def heal():
+    pass
+
+
+
+
+
 number_of_heroes = int(input())
 
 party = {}
@@ -5,4 +24,21 @@ party = {}
 for _ in range(number_of_heroes):
     hero_name, hit_points, mana_points = input().split()
     party[hero_name] = [int(hit_points), int(mana_points)]
-    
+
+command = input()
+while command != "End":
+    parts = command.split(" – ")
+    action = parts[0]
+    current_hero = parts[1]
+
+    if action == "CastSpell":
+        pass
+    elif action == "TakeDamage":
+        pass
+    elif action == "Recharge":
+        pass
+    elif action == "Heal":
+        pass
+
+
+    command = input()
