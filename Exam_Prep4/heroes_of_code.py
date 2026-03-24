c33a4e5b198bb97dd669b37fb02293f7ddd5c2ad
Georgi Angelov <gtangelov76@gmail.com>
2026-03-24 11:30:02 +0200

    Update heroes_of_code.py

--- Exam_Prep4/heroes_of_code.py
+++ Exam_Prep4/heroes_of_code.py
@@ -1,2 +1,8 @@
 number_of_heroes = int(input())
 
+party = {}
+
+for _ in range(number_of_heroes):
+    hero_name, hit_points, mana_points = input().split()
+    party[hero_name] = [int(hit_points), int(mana_points)]
+    
