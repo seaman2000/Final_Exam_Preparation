39157d3b6157f34fc8bef27555f6280b392626e3
Georgi Angelov <gtangelov76@gmail.com>
2026-03-24 13:38:26 +0200

    Update emoji_detector.py

--- Exam_Prep5/emoji_detector.py
+++ Exam_Prep5/emoji_detector.py
@@ -4,9 +4,6 @@ text_with_emojis = input()
 
 pattern = r"(:{2}|\*{2})([A-Z][a-z]{2,})\1"
 
-valid_emojis = re.findall(pattern, text_with_emojis)
-valid_emojis = [item[1] for item in valid_emojis]
-
 digits = re.findall(r"\d", text_with_emojis)
 
 cool_threshold = 1
@@ -15,13 +12,22 @@ for digit in digits:
     cool_threshold *= int(digit)
 
 cool_emojis = []
-for emoji in valid_emojis:
+all_emojis = []
+matches = re.finditer(pattern, text_with_emojis)
+
+for match in matches:
+    full_emoji = match.group(0)
+    emoji = match.group(2)
+
+    all_emojis.append(full_emoji)
+
     emoji_value = 0
     for char in emoji:
         emoji_value += ord(char)
+
     if emoji_value >= cool_threshold:
-        cool_emojis.append(emoji)
+        cool_emojis.append(full_emoji)
 
 print(f"Cool threshold: {cool_threshold}")
-print(f"{len(valid_emojis)} emojis found in the text. The cool ones are:")
+print(f"{len(all_emojis)} emojis found in the text. The cool ones are:")
 print(f"{'\n'.join(cool_emojis)}")
