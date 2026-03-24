f658861c74ef7a2944c214227a57142f0712af0e
Georgi Angelov <gtangelov76@gmail.com>
2026-03-24 12:42:01 +0200

    Update activation_keys.py

---
+++ Exam_Prep5/activation_keys.py
@@ -0,0 +1,32 @@
+def contains(activation_key: str, sub: str):
+    if sub in activation_key:
+        return f"{activation_key} contains {substring}"
+    return f"Substring not found!"
+
+
+def flip_upper_and_lower(activation_key: str, upper_lower: str, start: int, end: int):
+
+    
+
+
+def slice_():
+    pass
+
+
+
+raw_activation_key = input()
+
+command = input()
+while command != "Generate":
+    parts = command.split(">>>")
+    action = parts[0]
+    if action == "Contains":
+        substring = parts[1]
+        print(contains(raw_activation_key, substring))
+
+    elif action == "Flip":
+        upper_or_lower = parts[1]
+        start_idx = int(parts[2])
+        end_idx = int(parts[3])
+
+    command = input()
