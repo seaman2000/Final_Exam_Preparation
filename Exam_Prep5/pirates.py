9c1bfcab611e892fedd261671a89204d5e2542b2
Georgi Angelov <gtangelov76@gmail.com>
2026-03-24 15:46:27 +0200

    Update pirates.py

--- Exam_Prep5/pirates.py
+++ Exam_Prep5/pirates.py
@@ -1,9 +1,22 @@
-def plunder():
-    pass
+def plunder(target_cities:dict, current_town:str, killed_people:int, plundered_gold:int):
+    target_cities[current_town][0] -= killed_people
+    target_cities[current_town][1] -= plundered_gold
+    if target_cities[current_town][0] <= 0 or target_cities[current_town][1] <= 0:
+
+        del target_cities[current_town]
+        return (f"{current_town} plundered! {plundered_gold} gold stolen, {killed_people} citizens killed.\n"
+                f"{current_town} has been wiped off the map!")
+
+    return f"{current_town} plundered! {plundered_gold} gold stolen, {killed_people} citizens killed."
 
 
-def prosper():
-    pass
+
+def prosper(target_cities: dict, current_town: str, gold_increase: int):
+    if gold_increase < 0:
+        return f"Gold added cannot be a negative number!"
+    target_cities[current_town][1] += gold_increase
+    total_gold = target_cities[current_town][1]
+    return f"{gold_increase} gold added to the city treasury. {current_town} now has {total_gold} gold."
 
 
 command = input()
@@ -27,8 +40,17 @@ while event != "End":
     town = parts[1]
 
     if action == "Plunder":
-        pass
+        people = int(parts[2])
+        gold = int(parts[3])
+        print(plunder(cities, town, people, gold))
+
     elif action == "Prosper":
-        pass
+        gold = int(parts[2])
+        prosper()
 
     event = input()
+
+print(f"Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:")
+for city, values in cities.items():
+    population, gold = values
+    print(f"{city} -> Population: {population} citizens, Gold: {gold} kg")
