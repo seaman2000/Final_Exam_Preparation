3c4998ba00cfa25ceaea249b3241d4ebfe945cd3
Georgi Angelov <gtangelov76@gmail.com>
2026-03-28 09:25:31 +0200

    Update galactic_code.py

---
+++ Final_Exam/galactic_code.py
@@ -0,0 +1,46 @@
+coded_message = input()
+
+command = input()
+while command != "Finalize":
+    parts = command.split()
+    action = parts[0]
+    if action == "Encrypt":
+        coded_message = coded_message[::-1]
+        print(coded_message)
+    elif action == "Decrypt":
+        result = ''
+        for char in coded_message:
+            if char.islower():
+                result += char.upper()
+            elif char.isupper():
+                result += char.lower()
+        coded_message = result
+        print(coded_message)
+
+    elif action == "Substitute":
+        old = parts[1]
+        new = parts[2]
+        if old not in coded_message:
+            print("Character not found.")
+        else:
+            coded_message = coded_message.replace(old, new)
+            print(coded_message)
+
+    elif action == "Scramble":
+        index = int(parts[1])
+        char = parts[2]
+        if 0 <= index < len(coded_message):
+            coded_message = coded_message[:index] + char + coded_message[index + 1:]
+            print(coded_message)
+        else:
+            print("Index out of bounds.")
+
+    elif action == "Remove":
+        substring = parts[1]
+        if substring in coded_message:
+            coded_message = coded_message.replace(substring, '')
+
+    else:
+        print("Invalid command detected!")
+
+    command = input()
